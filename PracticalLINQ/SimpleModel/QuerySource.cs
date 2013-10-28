662ba71641d4d35dcf99151f48777bcdfe96e1b2
Bill Wagner <bill.w.wagner@outlook.com>
2013-10-28 07:07:12 -0700

    Create a query source

---
+++ PracticalLINQ/SimpleModel/QuerySource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleModel
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abstract { get; set; }
+
+        public virtual Presenter Presenter { get; set; }
+    }
+
+    public class Presenter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Session> Sessions { get; set; }
+
+    }
+
+    public class QuerySource : DbContext
+    {
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Presenter> Presenters { get; set; }
+    }
+}
