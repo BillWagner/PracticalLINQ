51721ab515ae8f87fefd33093b25b83056e023fd
Bill Wagner <bill.w.wagner@outlook.com>
2013-10-28 06:03:27 -0700

    Demo 1 AnyVsCount

---
+++ PracticalLINQ/PracticalLINQ/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnyVsCount();
+
+        }
+
+        private static void AnyVsCount()
+        {
+            var stopwatch = new Stopwatch();
+
+            var oddNumberSequence = (from value in GenerateLongRandomSequence()
+                                     where value % 2 == 1
+                                     select value);
+
+            stopwatch.Start();
+            var hasOdds = oddNumberSequence.Any();
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed time for Any: {0}", stopwatch.Elapsed);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            hasOdds = oddNumberSequence.Count() > 0;
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed time for Any: {0}", stopwatch.Elapsed);
+        }
+
+        public static IEnumerable<int> GenerateLongRandomSequence()
+        {
+            var generator = new Random();
+
+            return from n in Enumerable.Range(0, 5000000)
+                   select generator.Next(0, 1500);
+        }
+
+    }
+}
